a155816ff0f042104c3506cb527a5c445b76c416
tofimega <fintaistvan05@gmail.com>
2024-05-14 23:31:03 +0200

    game manager changes

--- scripts/managers/game_manager.gd
+++ scripts/managers/game_manager.gd
@@ -7,16 +7,51 @@ var active_explosions: int=0
 var active_bombs: int=0
 
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	pass # Replace with function body.
+	_setup_score_counters()
+	_load_level()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _setup_score_counters()->void:
 	pass
 
-func _load_level():
+func _load_level()->void:
 	pass
+#region incs, decs
+func add_player()->void:
+	pass
+func rem_player()->void:
+	pass
+	_check_game_state()
 
-func
+func inc_bombs()->void:
+	active_bombs+=1
+	
+func dec_bombs()->void:
+	active_bombs-=1
+	_check_game_state()
+	
+func inc_explosions()->void:
+	active_explosions+=1
+func dec_explosions()->void:
+	active_explosions-=1
+	_check_game_state()
+#endregion
+#region game state
+func next(over: bool)->void:
+	if over:
+		pass #TODO: results scene
+		return
+	pass #TODO: new game
+	
+func _check_game_state()->void:
+	if active_bombs==0 and active_explosions==0:
+		
+		if active_players.size()==1:
+			var over: bool=GlobalAccess.game_over(GlobalAccess.player_id.P1) # TODO: replace when players are added
+			next(over)
+			return
+			
+		elif active_players.is_empty():
+			next(false)
+#endregion
