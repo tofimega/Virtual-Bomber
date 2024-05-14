a2653010698cf3d2932c59f1e524dd2fb8161446
tofimega <fintaistvan05@gmail.com>
2024-05-14 22:47:34 +0200

    added gitPlugin

--- scripts/globals/global_access.gd
+++ scripts/globals/global_access.gd
@@ -5,6 +5,9 @@ var player_progress: Array[int]=[0,0,0]
 
 # at the end of a given round, this will be called
 func game_over(p: player_id)->bool:
+	if p==null:
+		return false
+		
 	player_progress[p]+=1
 	return player_progress[p]>games_to_win
 
@@ -13,7 +16,7 @@ var games_to_win: int=0
 
 
 func get_winner()->player_id:
-	return player_id.find_key(player_progress.find(player_progress.max()))	
+	return player_id.find_key(player_progress.find(player_progress.max()))
 
 # amount of players in-game
 var players: int=2:
