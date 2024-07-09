0bcfbe7b6851253a4fdb8e33bf816a2023ff6f6f
tofimega <fintaistvan05@gmail.com>
2024-07-09 20:32:44 +0200

    almost missed it

--- scenes/state_machines/state.gd
+++ scenes/state_machines/state.gd
@@ -11,11 +11,11 @@ var machine: StateMachine:
 @export var idle_update: bool=true:
 	set(i):
 		idle_update=i
-		set_process(i)
+		set_process(i&&is_processing())
 @export var fixed_update: bool=false:
 	set(f):
 		fixed_update=f
-		set_physics_process(f)
+		set_physics_process(f&&is_physics_processing())
 
 signal switch_to(new_state: State)
 
