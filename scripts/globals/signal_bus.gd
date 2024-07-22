1bf2923023d7f863290911c6c914106b0938c615
tofimega <fintaistvan05@gmail.com>
2024-07-22 13:42:20 +0200

    signal categorization

--- scripts/globals/signal_bus.gd
+++ scripts/globals/signal_bus.gd
@@ -1,12 +1,22 @@
 extends Node
 
-
+#region level loading stuff
 signal player_spawn_ready(spawm_point: PlayerSpawn)
 signal level_loaded
+signal enemy_spawn_ready(spawn_point: EnemySpawn)
+#endregion
 
+#region game settings stuff
 signal player_data_changed(id: GlobalAccess.PLAYER_ID)
 
+signal request_add_player()
+signal request_remove_player(player: GlobalAccess.PLAYER_ID)
+signal player_removed(player: GlobalAccess.PLAYER_ID)
+signal player_name_changed(player: GlobalAccess.PLAYER_ID, new_name: String)
+#endregion
 
+
+#region game flow
 signal bomb_placed(bomb: Bomb)
 signal bomb_exploded(bomb: Bomb)
 signal player_placing_bomb(bomb: Bomb)
@@ -17,16 +27,13 @@ signal explosion_dissipated()
 
 signal player_ready(p: Player)
 signal player_dead(p: Player)
+#endregion
 
-signal enemy_spawn_ready(spawn_point: EnemySpawn)
-
+#region scoring
 signal player_killed_player(killer: GlobalAccess.PLAYER_ID)
 signal player_killed_enemy(killer: GlobalAccess.PLAYER_ID)
+#endregion
 
-signal request_add_player()
-signal request_remove_player(player: GlobalAccess.PLAYER_ID)
-signal player_removed(player: GlobalAccess.PLAYER_ID)
-signal player_name_changed(player: GlobalAccess.PLAYER_ID, new_name: String)
 
 func emit_level_loaded()->void:
 	level_loaded.emit()
